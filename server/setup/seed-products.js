2477d4625f3ffc4ae2721fcbc12244edf230f697
Foyzul Karim <foyzul@mentorloop.com>
2022-01-25 09:39:34 +1100

    Started working on seeding data

--- server/setup/seed-products.js
+++ server/setup/seed-products.js
@@ -8,25 +8,10 @@ console.log('Seed starting');
 const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 console.log(uri);
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
-const connectWithDb = () => {
-  mongoose.connect(uri, options, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("Database connection established");
-      fs.readFile("./setup/products.json", (err, data) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const products = JSON.parse(data);
-          products.forEach(async (product) => {
-            const newProduct = await upsert(product);
-            console.log(newProduct);
-          });
-        }
-      });
-    }
-  });
+const connectWithDb = async () => {
+  const mongooseObj = await mongoose.connect(uri, options);
+  console.log("Connected to DB");
+  console.log('Press Ctrl+C to exit');
 };
 
 connectWithDb();
