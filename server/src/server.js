221db59052e285d1062547c81a0bbb02b65f6c78
Foyzul Karim <foyzulkarim@gmail.com>
2022-02-08 20:32:03 +1100

    event manager on going

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,24 @@
 const logger = require('pino')()
+const eventEmitter = require('./event-manager').getInstance();
 const app = require('./app');
+
+
+const broadcastDatabaseConnectionEstablished = (em) => {
+    em.emit('databaseConnectionEstablished');
+}
+
+const eventEmitterHealthCheck = (em) => {
+    em.on('databaseConnectionEstablished', () => {
+        logger.info('eventEmitterHealthCheck()=> Database connection established');
+    });
+}
 
 const connectWithDb = require("./mongo");
 
 const PORT = 5000;
-app.listen(PORT, () => {
-    connectWithDb();
+app.listen(PORT, async () => {
     console.log("server is running on port", PORT);
-    logger.info(`server is running on port ${PORT}`);
+    eventEmitterHealthCheck(eventEmitter);
+    await connectWithDb(broadcastDatabaseConnectionEstablished, eventEmitter);
+    logger.info(`Database connection established at ${new Date()}`);
 });
