3278d4bbc783891a76d1c9351eac59d458a5d658
Foyzul Karim <foyzulkarim@gmail.com>
2022-02-13 12:35:31 +1100

    clean up server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,44 +1,22 @@
 const PORT = 5000;
-const { setup } = require('./core');
-const { app, eventEmitter, connectWithDb, logger } = setup();
-const { init } = require('./modules')
+const { setup : setupCore } = require('./core');
+const { init } = require('./modules');
+const { handleError } = require('./common/middlewares');
 
-const initModules = async () => {
-    // console.log(__dirname);
-    await init(app);
+const initModules = async (app) => {
+    app = await init(app);
     return app;
 }
 
-
-const handleError = async (err, req, res, next) => {
-    if (res.headersSent) {
-        return next(err)
-    }
-
-    let code = 500;
-    if (err instanceof GeneralError) {
-        code = err.getCode();
-    }
-
-    let correlationId = req.headers['x-correlation-id'];
-    logger.error(err, { correlationId });
-    return res.status(code).json({
-        correlationId: correlationId, message: err.message
-    });
-}
-
 const configureRoutes = async (app) => {
-    // app.post("/api/users/register", handleValidation(validators.userSchemaValidate), userRegistrationHandler);
-    // app.use("/api/auth", authRoutes);
-    // app.use('/api', authenticateRequest, appRoutes);
-    // app.use('/health', healthHandler.healthHandler);
     app = await initModules(app);
-    // add default route 
     app.get('/', (req, res) => {
         res.send('Hello World!');
     });
     return app;
 }
+
+const { app, eventEmitter, connectWithDb, logger } = setupCore();
 
 try {
     configureRoutes(app);
@@ -48,8 +26,6 @@ catch (err) {
 }
 
 app.use(handleError);
-
-
 
 app.listen(PORT, async () => {
     console.log("server is running on port", PORT);
