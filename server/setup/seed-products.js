f4ba7b79d5a21409dd9371b5fab5222b78626924
Foyzul Karim <foyzulkarim@gmail.com>
2022-02-24 00:16:29 +1100

    Fixed url in product seed

--- server/setup/seed-products.js
+++ server/setup/seed-products.js
@@ -5,8 +5,8 @@ require('dotenv').config()
 const { save } = require("../src/modules/product/service");
 
 console.log('Seed starting');
-const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-console.log(uri);
+const isMongoDbUrl = process.env.IS_MONGODB_CLOUD_URL;
+const uri = isMongoDbUrl ? process.env.MONGODB_CLOUD_URL : `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const getFakeUniqueProductName = () => {
   return faker.fake("{{commerce.productName}} {{datatype.uuid}}");
