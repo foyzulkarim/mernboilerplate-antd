e28a67cac23825b66d5009eed9ac1490de82526c
Foyzul Karim <foyzulkarim@gmail.com>
2022-02-23 23:14:04 +1100

    added new docker-compose to use mongodb cloud url

--- server/setup/seed-users.js
+++ server/setup/seed-users.js
@@ -6,8 +6,9 @@ const { createUser, getByUsername } = require("../src/modules/auth/service");
 const data = require("./users.json");
 
 console.log('Seed starting');
-const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-console.log(uri);
+const isMongoDbUrl = process.env.IS_MONGODB_CLOUD_URL;
+const uri = isMongoDbUrl ? process.env.MONGODB_CLOUD_URL : `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 const connectWithDb = async () => {
     try {
@@ -16,7 +17,7 @@ const connectWithDb = async () => {
         await Promise.all(data.map(async (user) => {
             // check if user exists by username
             const userExists = await getByUsername(user.username);
-            if (!userExists) {             
+            if (!userExists) {
                 const savedUser = await createUser(user);
                 console.log(`Saved user id: ${savedUser._id}`);
             } else {
--- server/src/core/mongo.js
+++ server/src/core/mongo.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
-
-const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const isMongoDbUrl = process.env.IS_MONGODB_CLOUD_URL;
+const uri = isMongoDbUrl ? process.env.MONGODB_CLOUD_URL : `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true };
 const connectWithDb = async (cb, em) => {
-    console.log(`Connecting to mongoDB on server ${process.env.DB_HOST}`);
+    console.log(`Connecting to mongoDB...`);
     const connectionResult = await mongoose.connect(uri, options);
-    console.log(`Connected to mongoDB on ${process.env.DB_HOST}, database: ${connectionResult.connections[0].name} at ${new Date().toDateString()}`);
+    console.log(`Connected to mongoDB on database: ${connectionResult.connections[0].name} at ${new Date().toDateString()}`);
     cb && em && cb(em);
     console.log(`callback function executed at ${new Date()}`);
 };
