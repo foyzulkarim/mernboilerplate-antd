16a569f9730390570426f2e7eafed8df5bc69bf8
Belayet <belayet.hossain@apsissolutions.com>
2022-03-04 19:06:23 +0600

    unused declared/assign, console.log fix

--- server/src/modules/customer/service.js
+++ server/src/modules/customer/service.js
@@ -1,21 +1,22 @@
 // load repository.js functions
 const { save, update, getById, deleteById } = require("../../core/repository");
-const Model = require("./model");
-const { NotFound } = require("../../common/errors");
 const eventEmitter = require("../../core/event-manager").getInstance();
 
 const modelName = "Customer";
 
 const setupEventListeners = () => {
   eventEmitter.on(`${modelName}Created`, (model) => {
+    // eslint-disable-next-line no-console
     console.log(`${modelName} created`, model);
   });
 
   eventEmitter.on(`${modelName}Updated`, (model) => {
+    // eslint-disable-next-line no-console
     console.log(`${modelName} updated`, model);
   });
 
   eventEmitter.on(`${modelName}Deleted`, (model) => {
+    // eslint-disable-next-line no-console
     console.log(`${modelName} deleted`, model);
   });
 };
