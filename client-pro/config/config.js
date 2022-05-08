8c27bb88afb812aad092a42f24d9ff2286ee1c0a
Foyzul Karim <foyzulkarim@gmail.com>
2022-05-09 07:29:02 +1000

    Changed default language

--- client-pro/config/config.js
+++ client-pro/config/config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   // https://umijs.org/zh-CN/plugins/plugin-locale
   locale: {
     // default zh-CN
-    default: 'zh-CN',
+    default: 'en-US',
     antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
